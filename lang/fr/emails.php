c693a8f6da4894d751b201a0beedaf71303bf3b2
Mário Costa <mariocostaa6@gmail.com>
2025-11-13 22:46:17 +0000

    refactor: update language files for Movimentation (first batch)
    - Update English emails translations
    - Update English notifications translations
    - Update Spanish emails translations
    - Update Spanish notifications translations
    - Update French emails translations

--- lang/fr/emails.php
+++ lang/fr/emails.php
@@ -2,21 +2,21 @@
 
 return [
     // Email Subjects
-    'Transactions Created'                                                                                                                                                               => 'Transactions Créées',
-    'Transactions Deleted'                                                                                                                                                               => 'Transactions Supprimées',
+    'Transactions Created'                                                                                                                                                               => 'Movimentations Créées',
+    'Transactions Deleted'                                                                                                                                                               => 'Movimentations Supprimées',
     'Mareas Started'                                                                                                                                                                     => 'Mareas Démarrées',
     'Mareas Completed'                                                                                                                                                                   => 'Mareas Terminées',
     'System Notifications'                                                                                                                                                               => 'Notifications Système',
-    'Transaction Created'                                                                                                                                                                => 'Transaction Créée',
-    'Transaction Deleted'                                                                                                                                                                => 'Transaction Supprimée',
+    'Transaction Created'                                                                                                                                                                => 'Movimentation Créée',
+    'Transaction Deleted'                                                                                                                                                                => 'Movimentation Supprimée',
     'Marea Started'                                                                                                                                                                      => 'Marea Démarrée',
     'Marea Completed'                                                                                                                                                                    => 'Marea Terminée',
 
     // Email Content
     'Hello :name'                                                                                                                                                                        => 'Bonjour :name',
-    'New transactions have been created for vessel :vessel'                                                                                                                              => 'De nouvelles transactions ont été créées pour le navire :vessel',
-    'A new transaction has been created for vessel :vessel'                                                                                                                              => 'Une nouvelle transaction a été créée pour le navire :vessel',
-    'Transactions have been deleted from vessel :vessel'                                                                                                                                 => 'Des transactions ont été supprimées du navire :vessel',
+    'New transactions have been created for vessel :vessel'                                                                                                                              => 'De nouvelles movimentations ont été créées pour le navire :vessel',
+    'A new transaction has been created for vessel :vessel'                                                                                                                              => 'Une nouvelle movimentation a été créée pour le navire :vessel',
+    'Transactions have been deleted from vessel :vessel'                                                                                                                                 => 'Des movimentations ont été supprimées du navire :vessel',
     'New marea has been started for vessel :vessel'                                                                                                                                      => 'Une nouvelle marea a été démarrée pour le navire :vessel',
     'Marea has been completed for vessel :vessel'                                                                                                                                        => 'La marea a été terminée pour le navire :vessel',
     'View Details'                                                                                                                                                                       => 'Voir les Détails',
@@ -30,8 +30,8 @@ return [
     'Description'                                                                                                                                                                        => 'Description',
     'Income'                                                                                                                                                                             => 'Revenu',
     'Expense'                                                                                                                                                                            => 'Dépense',
-    'View Transactions'                                                                                                                                                                  => 'Voir les Transactions',
-    'These notifications have been grouped to avoid spam. The last :count transactions created are shown above.'                                                                         => 'Ces notifications ont été regroupées pour éviter le spam. Les dernières :count transactions créées sont affichées ci-dessus.',
+    'View Transactions'                                                                                                                                                                  => 'Voir les Movimentations',
+    'These notifications have been grouped to avoid spam. The last :count transactions created are shown above.'                                                                         => 'Ces notifications ont été regroupées pour éviter le spam. Les dernières :count movimentations créées sont affichées ci-dessus.',
 
     // Crew Member Invitations
     'Crew Member Invitation'                                                                                                                                                             => 'Invitation pour Équipage',
