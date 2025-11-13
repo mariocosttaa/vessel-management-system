3dcb0134cb506b975990b803652d691dd04e7104
Mário Costa <mariocostaa6@gmail.com>
2025-11-13 22:43:23 +0000

    feat: add crew position translation files
    - Add English translation file with all default crew positions
    - Add Portuguese translation file with all crew positions
    - Add Spanish translation file with all crew positions
    - Add French translation file with all crew positions

---
+++ lang/es/crew-positions.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    // Deck Department - Command & Navigation
+    'Captain'         => 'Capitán',
+    'First Officer'   => 'Primer Oficial',
+    'Second Officer'  => 'Segundo Oficial',
+    'Third Officer'   => 'Tercer Oficial',
+    'Chief Mate'      => 'Primer Oficial de Cubierta',
+    'Bosun'           => 'Contramaestre',
+    'Able Seaman'     => 'Marinero Calificado',
+    'Ordinary Seaman' => 'Marinero Ordinario',
+    'Deckhand'        => 'Marinero de Cubierta',
+
+    // Engine Department
+    'Chief Engineer'  => 'Jefe de Máquinas',
+    'Second Engineer' => 'Segundo Ingeniero',
+    'Third Engineer'  => 'Tercer Ingeniero',
+    'Fourth Engineer' => 'Cuarto Ingeniero',
+    'Engineer'        => 'Ingeniero',
+    'Mechanic'        => 'Mecánico',
+    'Electrician'     => 'Electricista',
+    'Oiler'           => 'Engrasador',
+    'Wiper'           => 'Limpiador',
+
+    // Galley & Service Department
+    'Cook'            => 'Cocinero',
+    'Chief Cook'      => 'Jefe de Cocina',
+    'Steward'         => 'Camarero',
+    'Chief Steward'   => 'Jefe de Camareros',
+
+    // Specialized Positions
+    'Radio Officer'   => 'Oficial de Radio',
+    'Purser'          => 'Sobrecargo',
+    'Carpenter'       => 'Carpintero',
+    'Welder'          => 'Soldador',
+    'Safety Officer'  => 'Oficial de Seguridad',
+    'Medical Officer' => 'Oficial Médico',
+    'Crane Operator'  => 'Operador de Grúa',
+    'Helmsman'        => 'Timonel',
+    'Lookout'         => 'Vigía',
+    'Trainee'         => 'Aprendiz',
+    'Cadet'           => 'Cadete',
+    'Supercargo'      => 'Sobrecarga',
+    'Pilot'           => 'Piloto',
+];
---
+++ lang/fr/crew-positions.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    // Deck Department - Command & Navigation
+    'Captain'         => 'Capitaine',
+    'First Officer'   => 'Premier Officier',
+    'Second Officer'  => 'Deuxième Officier',
+    'Third Officer'   => 'Troisième Officier',
+    'Chief Mate'      => 'Premier Lieutenant',
+    'Bosun'           => 'Maître d\'équipage',
+    'Able Seaman'     => 'Matelot Qualifié',
+    'Ordinary Seaman' => 'Matelot Ordinaire',
+    'Deckhand'        => 'Matelot de Pont',
+
+    // Engine Department
+    'Chief Engineer'  => 'Chef Mécanicien',
+    'Second Engineer' => 'Deuxième Mécanicien',
+    'Third Engineer'  => 'Troisième Mécanicien',
+    'Fourth Engineer' => 'Quatrième Mécanicien',
+    'Engineer'        => 'Mécanicien',
+    'Mechanic'        => 'Mécanicien',
+    'Electrician'     => 'Électricien',
+    'Oiler'           => 'Graisseur',
+    'Wiper'           => 'Nettoyeur',
+
+    // Galley & Service Department
+    'Cook'            => 'Cuisinier',
+    'Chief Cook'      => 'Chef Cuisinier',
+    'Steward'         => 'Steward',
+    'Chief Steward'   => 'Chef Steward',
+
+    // Specialized Positions
+    'Radio Officer'   => 'Officier Radio',
+    'Purser'          => 'Commissaire',
+    'Carpenter'       => 'Charpentier',
+    'Welder'          => 'Soudeur',
+    'Safety Officer'  => 'Officier de Sécurité',
+    'Medical Officer' => 'Officier Médical',
+    'Crane Operator'  => 'Grutier',
+    'Helmsman'        => 'Timonier',
+    'Lookout'         => 'Vigie',
+    'Trainee'         => 'Stagiaire',
+    'Cadet'           => 'Cadet',
+    'Supercargo'      => 'Surchargé',
+    'Pilot'           => 'Pilote',
+];
