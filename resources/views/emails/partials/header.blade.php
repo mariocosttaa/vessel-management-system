6195251230a992cccc9143560a498ea40e9f470d
Mário Costa <mariocostaa6@gmail.com>
2025-11-16 06:23:19 +0000

    feat: add vessel logo to email header
    - Display vessel logo on right side of email header when available
    - Keep Bindamy Marea logo on left side
    - Match PDF header layout pattern with conditional vessel logo display

--- resources/views/emails/partials/header.blade.php
+++ resources/views/emails/partials/header.blade.php
@@ -1,10 +1,15 @@
+@php
+    $vessel = $vessel ?? null;
+    $vesselLogoUrl = $vessel && $vessel->logo ? $vessel->logo_url : null;
+@endphp
+
 <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="background-color: #ffffff;">
     <tr>
         <td style="padding: 30px 40px 20px 40px;">
             <!-- Logo and Company Name -->
             <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                 <tr>
-                    <td align="left" valign="middle">
+                    <td align="left" valign="middle" width="{{ $vesselLogoUrl ? '50%' : '100%' }}">
                         <table role="presentation" cellspacing="0" cellpadding="0" border="0">
                             <tr>
                                 <td align="left" valign="middle">
@@ -13,6 +18,17 @@
                             </tr>
                         </table>
                     </td>
+                    @if($vesselLogoUrl)
+                        <td align="right" valign="middle" width="50%">
+                            <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="right">
+                                <tr>
+                                    <td align="right" valign="middle">
+                                        <img src="{{ $vesselLogoUrl }}" alt="{{ $vessel->name ?? 'Vessel Logo' }}" style="max-width: 150px; height: auto; display: block;" />
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    @endif
                 </tr>
             </table>
         </td>
